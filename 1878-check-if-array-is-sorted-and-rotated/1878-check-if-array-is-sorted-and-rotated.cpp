2c3fc73f35323a65afd6f48ac4387445722626c6
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2025-02-02 12:32:50 +0530

    Time: 0 ms (100%), Space: 10.9 MB (98.99%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,35 +1,51 @@
+// class Solution {
+//     bool helper(int i,vector<int> &nums){
+//         vector<int> v1,v2;
+// for(int j=0;j<=i;j++){
+//     v1.push_back(nums[j]);
+// }
+// for(int j=i+1;j<nums.size();j++){
+//     v2.push_back(nums[j]);
+// }
+// vector<int> v;
+// for(int j=0;j<v2.size();j++){
+// v.push_back(v2[j]);
+// }
+
+// for(int j=0;j<v1.size();j++){
+// v.push_back(v1[j]);
+// }
+// for(int j=0;j<v.size()-1;j++){
+//     if(v[j]>v[j+1])return false;
+//     }
+//     return true;
+// }
+
+// public:
+//     bool check(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1)return true;
+//         for(int i=0;i<n-1;i++){
+//             if(nums[i]>nums[i+1]){
+// return helper(i,nums);
+//             }
+//         }
+//         return true;
+//     }
+// };
 class Solution {
-    bool helper(int i,vector<int> &nums){
-        vector<int> v1,v2;
-for(int j=0;j<=i;j++){
-    v1.push_back(nums[j]);
-}
-for(int j=i+1;j<nums.size();j++){
-    v2.push_back(nums[j]);
-}
-vector<int> v;
-for(int j=0;j<v2.size();j++){
-v.push_back(v2[j]);
-}
-
-for(int j=0;j<v1.size();j++){
-v.push_back(v1[j]);
-}
-for(int j=0;j<v.size()-1;j++){
-    if(v[j]>v[j+1])return false;
-    }
-    return true;
-}
-
 public:
-    bool check(vector<int>& nums) {
+     bool check(vector<int>& nums) {
         int n=nums.size();
-        if(n==1)return true;
-        for(int i=0;i<n-1;i++){
-            if(nums[i]>nums[i+1]){
-return helper(i,nums);
+        int cnt=0;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                cnt++;
             }
         }
-        return true;
+        if(nums[n-1]>nums[0]){
+            cnt++;
+        }
+        return cnt<=1;
     }
 };
