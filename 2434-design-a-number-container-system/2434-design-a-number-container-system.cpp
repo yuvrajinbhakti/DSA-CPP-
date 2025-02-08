332912434641ee472c73a895e8126ae75d9579fc
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2025-02-08 10:34:36 +0530

    Time: 119 ms (56.56%), Space: 235.8 MB (5%) - LeetHub

---
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -0,0 +1,60 @@
+// class NumberContainers {
+// public:
+//     map<int, int> mp;
+//     NumberContainers() 
+//     {
+
+//     }
+    
+//     void change(int index, int number)
+//     {
+//         mp[index] = number;
+//     }
+    
+//     int find(int number)
+//     {
+//        for(auto it: mp){
+//            if(it.second == number) return it.first;
+//        }
+        
+//        return -1;
+//     }
+// };
+class NumberContainers {
+public:
+    unordered_map<int, int> mp;
+    unordered_map<int, set<int>> idx;
+    NumberContainers() {
+        mp.reserve(100000);
+    }
+    
+    void change(int index, int number) {
+        if (mp.count(index)){
+            int x=mp[index];
+            idx[x].erase(index); 
+            if (idx[x].size()==0) idx.erase(x);
+        }
+        mp[index]=number;
+        idx[number].insert(index);
+    }
+    
+    int find(int number) {
+        if (idx.count(number)==0) 
+            return -1;
+        return *(idx[number].begin());
+    }
+};
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
