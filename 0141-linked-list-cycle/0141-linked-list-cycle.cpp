d140f3ceea42fc0e9b48c77a38a30e7fce1fff07
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2025-02-18 19:47:51 +0530

    Time: 6 ms (90.81%), Space: 12 MB (24.53%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -22,17 +22,31 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+
+//         if(head==NULL || head->next==NULL )return false;
+//         ListNode* slow =head,*fast=head;
+//         while(fast->next!=NULL && fast->next->next!=NULL){
+//             fast=fast->next->next;
+//             slow=slow->next;
+//             if(fast==slow) return true;
+//         }
+// return false;
+//     }
+
+// };
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-
-        if(head==NULL || head->next==NULL )return false;
-        ListNode* slow =head,*fast=head;
-        while(fast->next!=NULL && fast->next->next!=NULL){
-            fast=fast->next->next;
+   bool hasCycle(ListNode *head) {
+        if(head==NULL || head->next==NULL) return false;
+        ListNode * slow=head,*fast=head;
+        while(fast!=NULL && fast->next!=NULL){
             slow=slow->next;
-            if(fast==slow) return true;
+            fast=fast->next->next;
+            if(slow==fast)return true;
         }
-return false;
+        return false;
     }
 };
