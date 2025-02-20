677b3685a7cd9d2998a1b9ebd75a3b5fcd04e91d
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2025-02-20 19:27:01 +0530

    Time: 0 ms (100%), Space: 12.7 MB (68.65%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,47 @@
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+// vector<int> ans;
+// for(int i=0;i<nums1.size();i++){
+//     int found=-1, currElement=nums1[i];
+//   for(int j=0;j<nums2.size();j++){
+//     if(nums2[j]==currElement){
+//     for(int k=j;k<nums2.size();k++){
+// if(nums2[k]>nums2[j]){
+//     found=nums2[k];
+//     break;
+// }
+//     }
+//     }
+
+//   }
+//     if(found!=-1){
+// ans.push_back(found);
+//     }
+//     else{
+//         ans.push_back(-1);
+//     }
+// } 
+
+// return ans;
+//     }
+// };
+
+
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-vector<int> ans;
-for(int i=0;i<nums1.size();i++){
-    int found=-1, currElement=nums1[i];
-  for(int j=0;j<nums2.size();j++){
-    if(nums2[j]==currElement){
-    for(int k=j;k<nums2.size();k++){
-if(nums2[k]>nums2[j]){
-    found=nums2[k];
-    break;
+stack<int> st;
+unordered_map<int,int> mpp;
+for(int n: nums2){
+    while(st.size() && st.top()<n){
+        mpp[st.top()]=n;
+        st.pop();
+    }
+    st.push(n);
 }
-    }
-    }
-
-  }
-    if(found!=-1){
-ans.push_back(found);
-    }
-    else{
-        ans.push_back(-1);
-    }
-} 
-
+vector<int> ans;
+for(int n: nums1)ans.push_back(mpp.count(n) ? mpp[n] : -1);
 return ans;
     }
 };
