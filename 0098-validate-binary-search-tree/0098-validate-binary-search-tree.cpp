8f6d339458238a171726d9483326aeb42baa5c2f
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2025-03-03 18:46:09 +0530

    Time: 2 ms (7.83%), Space: 22 MB (48.36%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,17 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+// bool helper(TreeNode* root, long long mini,long long maxi){
+//     if(root==NULL)return true;
+//     if(root->val >= maxi || root->val <=mini)return false;
+//     bool left=helper(root->left,mini,root->val);
+//     bool right=helper(root->right,root->val,maxi);
+//     return left && right;
+// }
+//     bool isValidBST(TreeNode* root) {
+//         if(root==NULL || (root->left==NULL && root->right==NULL))return true;
+//         return helper(root,LLONG_MIN,LLONG_MAX);
+//     }
+// };
+
+
 class Solution {
 public:
-bool helper(TreeNode* root, long long mini,long long maxi){
-    if(root==NULL)return true;
-    if(root->val >= maxi || root->val <=mini)return false;
-    bool left=helper(root->left,mini,root->val);
-    bool right=helper(root->right,root->val,maxi);
-    return left && right;
-}
     bool isValidBST(TreeNode* root) {
-        if(root==NULL || (root->left==NULL && root->right==NULL))return true;
-        return helper(root,LLONG_MIN,LLONG_MAX);
+        TreeNode* prev = NULL;
+        return validate(root, prev);
+    }
+    bool validate(TreeNode* node, TreeNode* &prev) {
+        if (node == NULL) return true;
+        if (!validate(node->left, prev)) return false;
+        if (prev != NULL && prev->val >= node->val) return false;
+        prev = node;
+        return validate(node->right, prev);
     }
 };
