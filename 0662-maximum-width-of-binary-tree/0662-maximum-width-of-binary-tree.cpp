51735523e11462f7719c37ee7eca592a16a2cb3a
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2025-03-04 19:46:48 +0530

    Time: 1 ms (21.26%), Space: 19.7 MB (35.67%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,26 +12,28 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-       if(root==NULL)return 0;
-       long long res=1;
-       queue<pair<TreeNode*,long long>> q;
-       q.push({root,0});
-       while(!q.empty()){
-        long long cnt=q.size();
-        long long start=q.front().second;
-        long long end= q.back().second;
-        res=max(res,end-start+1);
-        for(int i=0;i<cnt;i++){
-            pair<TreeNode* ,int> p=q.front();
-            long long indx=p.second-start;
+        if(root==NULL)return 0;
+        long long res=1;
+        queue<pair<TreeNode*, long long >> q;
+        q.push({root,0});
+        while(!q.empty()){
+           long long cnt=q.size();
+           //start is index of root node for first level
+           long long start=q.front().second; 
+           long long end=q.back().second;
+           res=max(res,end-start+1);
+           for(int i=0;i<cnt;i++){
+            pair<TreeNode*,int> p=q.front();
+            //we will use it while inserting it childern
+            //left child will be 2* index + 1 , right child will be 2*index + 2
+            long long index=p.second-start;
             q.pop();
-
-            if(p.first->left!=NULL)
-                q.push({p.first->left,(long long)2*indx+1});
-            if(p.first->right!=NULL)
-            q.push({p.first->right,(long long)2*indx+2});
+            //if left child exist
+            if(p.first->left!=NULL)q.push({p.first->left,(long long)2*index+1});
+            //if right child exist
+            if(p.first->right!=NULL)q.push({p.first->right,(long long)2*index+2});
+           }
         }
-       }
-       return res;
+        return res;
     }
 };
