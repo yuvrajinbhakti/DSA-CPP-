4549caac923e107b52835e26678ac2d1fe63468c
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2025-03-06 18:48:29 +0530

    Time: 0 ms (100%), Space: 65 MB (89.66%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -8,15 +8,33 @@ public:
         // }
         // return -1;
 
-        //method2 -> use hashmap(or array)
-        int n=nums.size();
-            vector<int> v(n,-1);
-            for(int i=0;i<n;i++){
-                if(v[nums[i]]==-1){
-                    v[nums[i]]=0;
-                }
-                else return nums[i];
+        //method2 -> use hashmap(or array) T.C. ->O(N),S.C. -> O(N)
+        // int n=nums.size();
+        //     vector<int> v(n,-1);
+        //     for(int i=0;i<n;i++){
+        //         if(v[nums[i]]==-1){
+        //             v[nums[i]]=0;
+        //         }
+        //         else return nums[i];
+        //     }
+        //     return -1;
+
+
+        //method3 Two Pointers -> T.C. -> O(N), S.C. -> O(1)
+        if(nums.size()>1){
+            int slow=nums[0];
+            int fast=nums[nums[0]];
+            while(slow!=fast){
+                slow=nums[slow];
+                fast=nums[nums[fast]];
             }
-            return -1;
+            fast=0;
+            while(fast!=slow){
+                fast=nums[fast];
+                slow=nums[slow];
+            }
+            return slow;
+        }
+        return -1;
     }
 };
