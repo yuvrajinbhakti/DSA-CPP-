606020bd04769bdaaa315675375061327988c9ee
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2025-04-17 20:42:49 +0530

    Time: 5 ms (3.85%), Space: 16.1 MB (7.3%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,15 +1,36 @@
+// class Solution {
+// public:
+//     int countPairs(vector<int>& nums, int k) {
+//         int cnt=0;
+//         int n=nums.size();
+//         for(int i=0;i<n-1;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(nums[i]==nums[j] && ((long long)i*j)%k==0){
+//                     cnt++;
+//                 }
+//             }
+//         }
+//         return cnt;
+//     }
+// };
+
+//method2
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
-        int cnt=0;
-        int n=nums.size();
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j] && ((long long)i*j)%k==0){
-                    cnt++;
-                }
+       unordered_map<int,vector<int>> valueToIndices;
+       int cnt=0;
+       for(int i=0;i<nums.size();i++){
+        valueToIndices[nums[i]].push_back(i);
+       }
+       for(auto & entry:valueToIndices){
+        vector<int>& indices=entry.second;
+        for(int i=0;i<indices.size()-1;++i){
+            for(int j=i+1;j<indices.size();++j){
+                if((1LL*indices[i]*indices[j])%k==0)cnt++;
             }
         }
-        return cnt;
+       }
+       return cnt;
     }
 };
