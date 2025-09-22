e8db2405427f402a9109cc69cc0709221aae0696
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2025-09-22 08:50:57 +0530

    Time: 1 ms (36.53%), Space: 23.1 MB (89.81%) - LeetHub

---
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.cpp
@@ -0,0 +1,40 @@
+/*
+
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        int freq[101]={0}, maxF=0;
+        for (int x: nums){
+            freq[x]++;
+            maxF=max(maxF, freq[x]);
+        }
+        int ans=0;
+        for (int f: freq){
+            if (f==maxF)
+                ans+=f;
+        }
+        return ans;
+    }
+};
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
+*/
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        int freq[101]={0}, maxF=0, f=0;
+        for (int x: nums){
+            freq[x]++;
+            f+=(freq[x]==maxF);
+            if (freq[x]> maxF) f=1, maxF=freq[x];
+        }
+        return f*maxF;
+    }
+};
