8294a7590d2d74a194e2b6a611a4a12ec09fb6cc
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-08 22:27:38 +0530

    Time: 0 ms (100%), Space: 14.3 MB (32.52%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,21 +1,51 @@
 //T.C. = O(N^2),S.C. = O(N)
+// class Solution {
+// public:
+//     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+//         int size=nums.size();
+//         vector<int> ans(size);
+//         for(int i=0;i<size;i++){
+//             int cnt=0;
+//             for(int j=0;j<size;j++){
+//                 if( nums[j]<nums[i] ){
+//                     cnt++;
+//                 }
+//             }
+//             ans[i]=cnt;
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+// Time: O(n + k) → k = 101 ≈ constant
+// Space: O(k) → constant
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        int size=nums.size();
-        vector<int> ans(size);
-        for(int i=0;i<size;i++){
-            int cnt=0;
-            for(int j=0;j<size;j++){
-                if(i!=j && nums[j]<nums[i] ){
-                    cnt++;
-                }
-            }
-            ans[i]=cnt;
+        // Since nums[i] is between 0 and 100
+        vector<int> freq(101, 0);
+
+        // Count frequency of each number
+        for (int num : nums) {
+            freq[num]++;
         }
+
+        // Prefix sum:
+        // freq[i] will store how many numbers are < i
+        for (int i = 1; i <= 100; i++) {
+            freq[i] += freq[i - 1];
+        }
+
+        vector<int> ans;
+        for (int num : nums) {
+            if (num == 0)
+                ans.push_back(0);
+            else
+                ans.push_back(freq[num - 1]);
+        }
+
         return ans;
     }
 };
-
-
-
