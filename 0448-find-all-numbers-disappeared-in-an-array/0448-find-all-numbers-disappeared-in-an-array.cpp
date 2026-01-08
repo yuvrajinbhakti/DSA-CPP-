e2c7bb4b317be72e8e53aac057322be9be80df45
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-08 23:46:39 +0530

    Time: 0 ms (100%), Space: 52.9 MB (97.59%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -1,15 +1,57 @@
+// class Solution {
+// public:
+//     vector<int> findDisappearedNumbers(vector<int>& nums) {
+//         vector<int> ans;
+//         int size=nums.size();
+//         vector<int> freq(size+1);
+//         for(int i=0;i<size;i++){
+//             freq[nums[i]]++;
+//         }
+//         for(int i=1;i<freq.size();i++){
+//             if(freq[i]==0)ans.push_back(i);
+//         }
+//         return ans;
+//     }
+// };
+
+//if inplace allowed
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> ans;
-        int size=nums.size();
-        vector<int> freq(size+1);
-        for(int i=0;i<size;i++){
-            freq[nums[i]]++;
+
+        // Mark visited numbers
+        for (int i = 0; i < nums.size(); i++) {
+            int idx = abs(nums[i]) - 1;
+            if (nums[idx] > 0) {
+                nums[idx] = -nums[idx];
+            }
         }
-        for(int i=1;i<freq.size();i++){
-            if(freq[i]==0)ans.push_back(i);
+
+        // Collect indices that were never marked
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > 0) {
+                ans.push_back(i + 1);
+            }
+        }
+
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        unordered_set<int> arr(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<int> ans;
+        for(int i = 1; i <= n; i++) {
+            if(arr.find(i) == arr.end()) {
+                ans.push_back(i);
+            }
         }
         return ans;
     }
 };
+*/
