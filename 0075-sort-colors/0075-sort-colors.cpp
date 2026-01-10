375c2f6c42df65e48a1e5e8b1f9bcc22661764b7
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-10 22:29:33 +0530

    Time: 0 ms (100%), Space: 11.5 MB (96.83%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -5,31 +5,57 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int zeroCnt=0,oneCnt=0,twoCnt=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==0){
+//                zeroCnt++;
+//             }
+//             else if(nums[i]==1){
+// oneCnt++;
+//             }
+//             else{
+//                 twoCnt++;
+//             }
+//         }
+//         for(int i=0;i<nums.size();i++){
+//             if(zeroCnt){
+//                 nums[i]=0;
+//                 zeroCnt--;
+//             }
+//             else if(oneCnt){
+//                 nums[i]=1;
+//                 oneCnt--;
+//             }
+//             else nums[i]=2;
+//         }
+//     }
+// };
+
+// Dutch National Flag algorithm
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zeroCnt=0,oneCnt=0,twoCnt=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-               zeroCnt++;
+        int low=0,mid=0,high=nums.size()-1;
+        //0 to mid-1
+        //mid
+        // mid+1 to high
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]);
+                mid++;
+                low++;
             }
-            else if(nums[i]==1){
-oneCnt++;
+            else if(nums[mid]==1){
+                mid++;
             }
-            else{
-                twoCnt++;
+            else {
+                swap(nums[mid],nums[high]);
+                high--;
             }
-        }
-        for(int i=0;i<nums.size();i++){
-            if(zeroCnt){
-                nums[i]=0;
-                zeroCnt--;
-            }
-            else if(oneCnt){
-                nums[i]=1;
-                oneCnt--;
-            }
-            else nums[i]=2;
         }
     }
 };
