3a35d0a371f8e5f9450c1d075f58a8643643c4a5
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-10 12:58:58 +0530

    Time: 2 ms (43.68%), Space: 19 MB (6.04%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<string> st;
+        //if operation like +-*... then take last 2 numbers from stack and apply that operation
+        //if number then push in stack
+        int size=tokens.size();
+        for(int i=0;i<size;i++){
+            if(tokens[i]=="+" || tokens[i]=="-" || tokens[i]=="*" || tokens[i]=="/"){
+                int number1=stoi(st.top());
+                st.pop();
+                int number2=stoi(st.top());
+                st.pop();
+                int res=0;
+                if(tokens[i]=="+"){
+                    res=number2+number1;
+                }
+                else if(tokens[i]=="-"){
+                    res=number2-number1;        // doing numbers 2 first because stack if lifo
+                }
+                else if(tokens[i]=="*"){
+                    res=number2*number1;
+                }
+                else if(tokens[i]=="/"){
+                    res=number2/number1;
+                }
+                st.push(to_string(res));
+            }
+            else{
+                st.push(tokens[i]);
+            }
+        }
+        return stoi(st.top());
+    }
+};
