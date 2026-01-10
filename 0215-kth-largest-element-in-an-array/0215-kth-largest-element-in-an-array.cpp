16935d48d2b818583808d359ef4c2778df6e41a8
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-10 21:43:46 +0530

    Time: 31 ms (59.62%), Space: 61.7 MB (31.49%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,3 +1,4 @@
+//T.C. = o(n log n)
 // class Solution {
 // public:
 //     int findKthLargest(vector<int>& nums, int k) {
@@ -6,13 +7,28 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int findKthLargest(vector<int>& nums, int k) {
+//         priority_queue<int> pq;
+//         // if manually pushing then it will again be t.c. of N Log N as log N for inserting N times
+//         for(int i=0;i<nums.size();i++){
+//             pq.push(nums[i]);
+//         }
+//         while(k>1){
+//             pq.pop();
+//             k=k-1;
+//         }
+//         return pq.top();
+//     }
+// };
+
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int> pq;
-        for(int i=0;i<nums.size();i++){
-            pq.push(nums[i]);
-        }
+        priority_queue<int> pq(nums.begin(),nums.end());
+     
+        
         while(k>1){
             pq.pop();
             k=k-1;
