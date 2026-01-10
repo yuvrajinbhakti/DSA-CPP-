5132c139c4c6d80dae5c68e4df1754be07ebe2e5
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-10 22:16:32 +0530

    Time: 0 ms (100%), Space: 11.7 MB (46.77%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,35 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//     }
+// };
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int zeroCnt=0,oneCnt=0,twoCnt=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+               zeroCnt++;
+            }
+            else if(nums[i]==1){
+oneCnt++;
+            }
+            else{
+                twoCnt++;
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(zeroCnt){
+                nums[i]=0;
+                zeroCnt--;
+            }
+            else if(oneCnt){
+                nums[i]=1;
+                oneCnt--;
+            }
+            else nums[i]=2;
+        }
     }
 };
