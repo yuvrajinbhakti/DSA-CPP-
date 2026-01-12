aa9686a7d825705a3162b408a2f6c744a59abd52
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-12 19:10:02 +0530

    Time: 108 ms (6.79%), Space: 95.1 MB (5.7%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,22 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         if(nums.size()==1)return false;
+//         sort(nums.begin(),nums.end());
+//         for(int i=0;i<nums.size()-1;i++){
+//             if(nums[i]==nums[i+1])return true;
+//         }
+//         return false;
+//     }
+// };
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        if(nums.size()==1)return false;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1])return true;
-        }
-        return false;
+       set<int> st;
+       for(int i=0;i<nums.size();i++){
+        st.insert(nums[i]);
+       }
+       return !(nums.size()==st.size());
     }
 };
