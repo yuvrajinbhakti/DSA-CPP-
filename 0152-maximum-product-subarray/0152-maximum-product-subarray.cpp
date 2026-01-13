47744bacadfed0560962b2ce9b80a513a1ae846f
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-13 22:45:25 +0530

    Time: 0 ms (100%), Space: 17.8 MB (23.04%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -14,17 +14,46 @@
 // };
 
 
+// class Solution {
+// public:
+//     //O(N) time O(1) space
+
+//     int maxProduct(vector<int> A) {
+//         int n = A.size(), res = A[0], l = 0, r = 0;
+//         for (int i = 0; i < n; i++) {
+//             l =  (l ? l : 1) * A[i];
+//             r =  (r ? r : 1) * A[n - 1 - i];
+//             res = max(res, max(l, r));
+//         }
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
-    //O(N) time O(1) space
+    int maxProduct(vector<int>& nums) {
+        // maxProd: maximum product ending at current index
+        // minProd: minimum product ending at current index
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        int ans = nums[0];
 
-    int maxProduct(vector<int> A) {
-        int n = A.size(), res = A[0], l = 0, r = 0;
-        for (int i = 0; i < n; i++) {
-            l =  (l ? l : 1) * A[i];
-            r =  (r ? r : 1) * A[n - 1 - i];
-            res = max(res, max(l, r));
+        for (int i = 1; i < nums.size(); i++) {
+            int curr = nums[i];
+
+            // If current number is negative, max and min swap roles
+            if (curr < 0) {
+                swap(maxProd, minProd);
+            }
+
+            // Either start new subarray or extend previous one
+            maxProd = max(curr, maxProd * curr);
+            minProd = min(curr, minProd * curr);
+
+            // Update global answer
+            ans = max(ans, maxProd);
         }
-        return res;
+
+        return ans;
     }
 };
