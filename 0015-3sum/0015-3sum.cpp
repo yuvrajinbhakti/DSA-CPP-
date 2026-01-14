2e6c6b75ea382168d71d037d0feefcc543e8955d
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-14 22:53:14 +0530

    Time: 55 ms (32.53%), Space: 29 MB (70.4%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,90 @@
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         //O(N^3) - Brute Force -> 3 loops -> gives tle
+// vector<vector<int>> ans;
+// set<vector<int>> st;
+//         for(int i=0;i<nums.size();i++){
+//             for(int j=i+1;j<nums.size();j++){
+//                 for(int k=j+1;k<nums.size();k++){
+//                     if(nums[i]+nums[j]+nums[k]==0){
+//                         vector<int> vec={nums[i],nums[j],nums[k]};
+//                         sort(vec.begin(),vec.end());
+//                         st.insert(vec);
+//                     }
+//                 }
+//             }
+//         }
+
+//         for(auto v:st){
+//             ans.push_back(v);
+//         }
+// return ans;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     vector<vector<int>> threeSum(vector<int>& nums) {
+//         set<vector<int>> st;
+//         int n = nums.size();
+
+//         for (int i = 0; i < n; i++) {
+//             unordered_set<int> seen;
+
+//             for (int j = i + 1; j < n; j++) {
+//                 int third = -(nums[i] + nums[j]);
+
+//                 if (seen.count(third)) {
+//                     vector<int> triplet = {nums[i], nums[j], third};
+//                     sort(triplet.begin(), triplet.end());
+//                     st.insert(triplet);
+//                 }
+//                 seen.insert(nums[j]);
+//             }
+//         }
+//         return vector<vector<int>>(st.begin(), st.end());
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
+        vector<vector<int>> ans;
+        int n = nums.size();
+
         sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.size() - 1;
+        for (int i = 0; i < n; i++) {
+            // skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
+            int l = i + 1, r = n - 1;
 
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.push_back({nums[i], nums[j], nums[k]});
-                    j++;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
 
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
-                    }
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+
+                    // skip duplicates
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
+
+                    l++;
+                    r--;
+                }
+                else if (sum < 0) {
+                    l++;
+                }
+                else {
+                    r--;
                 }
             }
         }
-        return res;        
+        return ans;
     }
 };
