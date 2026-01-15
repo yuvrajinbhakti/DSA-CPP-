71f32aed56ef8327c6da24050ee4ddb93961fa2f
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-15 22:34:05 +0530

    Time: 0 ms (100%), Space: 7.9 MB (30.21%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,18 @@
+// class Solution {
+// public:
+//     int getSum(int a, int b) {
+//         return a+b;
+//     }
+// };
+
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        while (b != 0) {
+            int carry = (a & b) << 1;  // carry
+            a = a ^ b;                // sum without carry
+            b = carry;
+        }
+        return a;
     }
 };
