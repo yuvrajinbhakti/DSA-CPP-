05fc04de9b64e222ca73837170ac4db36a2d93b7
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-17 20:56:08 +0530

    Time: 0 ms (100%), Space: 11.3 MB (18.61%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,19 +1,36 @@
+// class Solution {
+// public:
+//     int findOnesinNumber(int x){
+//     int cnt=0;
+//     while(x>0){
+//         if(x%2==1)cnt++;
+//         x=x/2;
+//     }
+//     return cnt;
+//     }
+//     vector<int> countBits(int n) {
+//         vector<int> ans;
+//         for(int i=0;i<=n;i++){
+//             int currOnesCnt= findOnesinNumber(i);
+//             ans.push_back(currOnesCnt);
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int findOnesinNumber(int x){
-    int cnt=0;
-    while(x>0){
-        if(x%2==1)cnt++;
-        x=x/2;
-    }
-    return cnt;
-    }
-    vector<int> countBits(int n) {
-        vector<int> ans;
-        for(int i=0;i<=n;i++){
-            int currOnesCnt= findOnesinNumber(i);
-            ans.push_back(currOnesCnt);
+    vector <int> countBits(int num) {
+        vector<int> res(num);
+        res.push_back(0);  // for num = 0
+        if (num == 0) return res;
+        
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 == 0) 
+                res[i] = res[i / 2];
+            else
+                res[i] = res[i - 1] + 1;
         }
-        return ans;
+        return res;
     }
 };
