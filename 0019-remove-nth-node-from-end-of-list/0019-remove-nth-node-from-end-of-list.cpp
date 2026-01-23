27f96478a7edc98ffa76da27e381890c5bca5561
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-23 23:28:28 +0530

    Time: 0 ms (100%), Space: 15.1 MB (24.69%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,21 +8,62 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         if(head==NULL || head->next==NULL)return NULL;
+//         ListNode* curr=head,*ahead=head;
+//         while(n>0){
+//             n--;
+//             ahead=ahead->next;
+//         }
+//         if(ahead == NULL) return head->next;        // delete head, as when n==length of linked list
+//         while(ahead!=NULL && ahead->next!=NULL){
+//             curr=curr->next;
+//             ahead=ahead->next;
+//         }
+//         if(curr->next!=NULL)curr->next=curr->next->next;
+//         return head;
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         int len=0;
+//         ListNode* curr=head;
+//         while(curr!=NULL){
+//             curr=curr->next;
+//             len++;
+//         }
+//         ListNode* NewCurr=head;
+//         int fromStart=len-n;
+//         if(fromStart==0)return head->next;
+//         for(int i=1;i<fromStart;i++){
+//             NewCurr=NewCurr->next;
+//         }
+//         if(NewCurr->next==NULL)NewCurr=NULL;
+//         else NewCurr->next=NewCurr->next->next;
+//         return head;
+//     }
+// };
+
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(head==NULL || head->next==NULL)return NULL;
-        ListNode* curr=head,*ahead=head;
-        while(n>0){
-            n--;
-            ahead=ahead->next;
+        ListNode* start=new ListNode();
+        start->next=head;
+        ListNode* slow=start,*fast=start;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
         }
-        if(ahead == NULL) return head->next;        // delete head, as when n==length of linked list
-        while(ahead!=NULL && ahead->next!=NULL){
-            curr=curr->next;
-            ahead=ahead->next;
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
         }
-        if(curr->next!=NULL)curr->next=curr->next->next;
-        return head;
+        slow->next=slow->next->next;
+        return start->next;
     }
 };
