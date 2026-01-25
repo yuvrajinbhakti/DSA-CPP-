68b76691abe80983e1056701f50cd36bf7fd5901
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-01-25 15:00:58 +0530

    Time: 7 ms (9.58%), Space: 17.2 MB (89.33%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        if (k == 1) return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int ans = INT_MAX;
+        for (int i = 0; i + k - 1 < nums.size(); i++) {
+            ans = min(ans, nums[i + k - 1] - nums[i]);
+        }
+
+        return ans;
+    }
+};
