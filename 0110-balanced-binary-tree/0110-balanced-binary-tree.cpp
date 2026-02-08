194ffdbb321691585527a196cc2e538d15c3f1df
Yuvraj Singh Nain <93258349+yuvrajinbhakti@users.noreply.github.com>
2026-02-08 12:06:28 +0530

    Time: 0 ms (100%), Space: 22.8 MB (99.94%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -1,3 +1,37 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+//     private:
+//     int height(TreeNode* node){
+//         if(node==NULL)return 0;
+//         int leftht=height(node->left);
+//         int rightht=height(node->right);
+//         return 1+max(leftht,rightht);
+//     }
+
+//     bool helper(TreeNode* root){
+//         if(root==NULL)return true;
+//         int leftHt=height(root->left);
+//         int rightHt=height(root->right);
+//         if( abs(rightHt-leftHt)>1)return false;
+//         return helper(root->left) && helper(root->right);
+//     }
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         return helper(root);
+//     }
+// };
+
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -11,22 +45,22 @@
  */
 class Solution {
     private:
-    int height(TreeNode* node){
-        if(node==NULL)return 0;
-        int leftht=height(node->left);
-        int rightht=height(node->right);
-        return 1+max(leftht,rightht);
-    }
+    int helperDfs(TreeNode* root){
+        if(root==NULL)return 0;
 
-    bool helper(TreeNode* root){
-        if(root==NULL)return true;
-        int leftHt=height(root->left);
-        int rightHt=height(root->right);
-        if( abs(rightHt-leftHt)>1)return false;
-        return helper(root->left) && helper(root->right);
+        int left=helperDfs(root->left);
+        if(left==-1)return -1;
+
+        int right=helperDfs(root->right);
+        if(right==-1)return -1;
+
+        if(abs(right-left)>1)return -1;
+        
+        return 1+max(left,right);
     }
 public:
     bool isBalanced(TreeNode* root) {
-        return helper(root);
+        return helperDfs(root)!=-1;
     }
 };
+
